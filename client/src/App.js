43d61d034ed498a7e168bfc688261e0ba72d0e89
Nick Woods <nicholas.andrew.woods@gmail.com>
2020-01-01 19:48:00 -0800

    add frontend scaffolding

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+      <h1>Saluton</h1>
+    </div>
+  );
+}
+
+export default App;

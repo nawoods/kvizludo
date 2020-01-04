c6ce40a0bca82b145cfdf10a14157afb47fe06c1
Nick Woods <nicholas.andrew.woods@gmail.com>
2020-01-03 20:39:03 -0800

    hook frontend and backend together for questionList

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -1,67 +1,63 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button, Table } from 'reactstrap';
+import { Container, ListGroupItem, Button, Table } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 
 class QuestionList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      questions: [
-        { 
-          id: uuid(),
-          questionText: 'En kiu lando estas Seatlo?',
-          correctAnswer: 'Usono',
-          incorrectAnswer1: 'Kanado',
-          incorrectAnswer2: 'Meksiko',
-          incorrectAnswer3: 'Svedio'
-        },
-        { 
-          id: uuid(),
-          questionText: 'Kia besto estas Galojo?',
-          correctAnswer: 'Kato',
-          incorrectAnswer1: 'Hundo',
-          incorrectAnswer2: 'Testudo',
-          incorrectAnswer3: 'Serpento'
-        },
-        { 
-          id: uuid(),
-          questionText: 'Kiu estas la ĉefurbo de Kanado?',
-          correctAnswer: 'Otavo',
-          incorrectAnswer1: 'Toronto',
-          incorrectAnswer2: 'Vankuvero',
-          incorrectAnswer3: 'Montrealo'
-        }
-      ]
-    }
+
+    this.state = { questions: [] };
+    this.fetchQuestions();
   }
+
+  fetchQuestions = () => {
+    fetch('/api/questions')
+      .then(res => res.json())
+      .then(questions => {
+        this.setState(state => ({
+          ...state,
+          questions
+        }));
+      });
+  };
 
   addQuestion = () => {
-    const questionText = prompt('Demando');
-    const correctAnswer = prompt('Ĝusta respondo');
-    const incorrectAnswer1 = prompt('Malĝusta respondo 1');
-    const incorrectAnswer2 = prompt('Malĝusta respondo 2');
-    const incorrectAnswer3 = prompt('Malĝusta respondo 3');
-    if (questionText && correctAnswer && incorrectAnswer1 &&
-                        incorrectAnswer2 && incorrectAnswer3) {
-      this.setState(state => ({
-        questions: [...state.questions, {
-          id: uuid(),
-          questionText,
-          correctAnswer,
-          incorrectAnswer1,
-          incorrectAnswer2,
-          incorrectAnswer3
-        }]
-      }))
-    }
-  }
+    const newQuestion = {
+      questionText: prompt('Demando'),
+      correctAnswer: prompt('Ĝusta respondo'),
+      incorrectAnswer1: prompt('Malĝusta respondo 1'),
+      incorrectAnswer2: prompt('Malĝusta respondo 2'),
+      incorrectAnswer3: prompt('Malĝusta respondo 3')
+    };
+
+    if (!newQuestion) return;
+
+    fetch('/api/questions', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newQuestion)
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.setState(state => ({
+          questions: [...state.questions, res]
+        }));
+      });
+  };
 
   removeQuestion = id => {
-    this.setState(state => ({
-      questions: state.questions.filter(question => question.id !== id)
-    }));
-  }
+    fetch(`/api/questions/${id}`, { method: 'DELETE' })
+      .then(res => res.json())
+      .then(res => {
+        if (!res.success) return;
+
+        this.setState(state => ({
+          questions: state.questions.filter(question => question._id !== id)
+        }));
+      });
+  };
 
   render() {
     const { questions } = this.state;
@@ -76,31 +72,33 @@ class QuestionList extends Component {
         </Button>
         <TransitionGroup className="question-list">
           {questions.map((question) => (
-            <CSSTransition key={question.id} timeout={500} classNames="fade">
+            <CSSTransition key={question._id} timeout={500} classNames="fade">
               <ListGroupItem>
                 <Button
                   className="remove-btn"
                   color="danger"
                   size="sm"
-                  onClick={() => this.removeQuestion(question.id)}
+                  onClick={() => this.removeQuestion(question._id)}
                 >&times;</Button>
                 {question.questionText}
                 <Table borderless size="sm">
                   <tbody>
-                    <th scope="row">Ĝusta</th>
-                    <td>{question.correctAnswer}</td>
-                  </tbody>
-                  <tbody>
-                    <th scope="row">Malĝusta</th>
-                    <td>{question.incorrectAnswer1}</td>
-                  </tbody>
-                  <tbody>
-                    <th scope="row">Malĝusta</th>
-                    <td>{question.incorrectAnswer2}</td>
-                  </tbody>
-                  <tbody>
-                    <th scope="row">Malĝusta</th>
-                    <td>{question.incorrectAnswer3}</td>
+                    <tr>
+                      <th scope="row">Ĝusta</th>
+                      <td>{question.correctAnswer}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Malĝusta</th>
+                      <td>{question.incorrectAnswer1}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Malĝusta</th>
+                      <td>{question.incorrectAnswer2}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Malĝusta</th>
+                      <td>{question.incorrectAnswer3}</td>
+                    </tr>
                   </tbody>
                 </Table>
               </ListGroupItem>
